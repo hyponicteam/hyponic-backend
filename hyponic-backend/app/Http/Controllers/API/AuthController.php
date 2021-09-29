babeed8f6a5cae64eaa10710d3d437606da7824c
Ivan Aulia Rahman <ivanar6542@gmail.com>
2021-09-29 15:24:42 +0700

    Auth API Fixes #1

---
+++ hyponic-backend/app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /**
+     * User register
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        if(!$fields) {
+            return ResponseFormatter::error(null, 'Invalid data input', 400);
+        }
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        if(!$user) {
+            return ResponseFormatter::error(null, 'Something went wrong', 500);
+        }
+
+        return ResponseFormatter::success([
+            'user' => $user,
+        ], 'Register succesful');
+    }
+
+    /**
+     * User login
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+
+        if(!$fields) {
+            return ResponseFormatter::error(null, 'Invalid data input', 400);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)) {
+            return ResponseFormatter::error(null, 'Bad credentials', 404);
+        }
+
+        $token = $user->createToken('access_token')->plainTextToken;
+
+        return ResponseFormatter::success([
+            'user' => $user,
+            'access_token' => $token
+        ], 'Login successful');
+    }
+
+    /**
+     * User logout
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $logout = Auth::user()->tokens()->delete();
+
+        if(!$logout) {
+            return ResponseFormatter::error(null, 'Something went wrong', 404);
+        }
+
+        return ResponseFormatter::success(null, 'Logout successful');
+    }
+}
+
--- hyponic-backend/routes/api.php
+++ hyponic-backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
 });
+
