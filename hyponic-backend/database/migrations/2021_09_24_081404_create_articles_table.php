406bfdaf46ade9a29287a3031eb07d4973688e88
Ivan Aulia Rahman <ivanar6542@gmail.com>
2021-10-02 17:26:52 +0700

    add articles migration and add role and profile picture fields to users

---
+++ hyponic-backend/database/migrations/2021_09_24_081404_create_articles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->longText('content');
+            $table->string('image_url');
+            $table->string('author');
+            $table->foreignId('article_category_id');
+            $table->foreignId('user_id');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
