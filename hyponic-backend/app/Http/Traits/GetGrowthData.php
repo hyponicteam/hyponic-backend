2f0f82ddc6c9dc4820c4b24dca0693daa6131314
Ivan Aulia Rahman <ivanar6542@gmail.com>
2021-11-21 15:12:41 +0700

    Fix insight API Fixes #28

--- hyponic-backend/app/Http/Traits/GetGrowthData.php
+++ hyponic-backend/app/Http/Traits/GetGrowthData.php
@@ -12,12 +12,13 @@ trait GetGrowthData
 
         for ($i = 0; $i < count($data) - 1; $i++) {
             $growth = round($data[$i]->{$column_name} - $data[$i + 1]->{$column_name}, 2);
-            $duration = round(Carbon::parse($data[$i]->updated_at)->diffInSeconds(Carbon::parse($data[$i + 1]->updated_at), true) / 86400, 2);
+            $duration = round(Carbon::parse($data[$i]->created_at)->diffInSeconds(Carbon::parse($data[$i + 1]->created_at), true) / 86400, 2);
+            
             $plant_height_growth = round($growth / $duration, 2);
 
             array_push($growth_per_days, [
-                'from' => Carbon::parse($data[$i + 1]->updated_at)->toDateTimeString(),
-                'to' => Carbon::parse($data[$i]->updated_at)->toDateTimeString(),
+                'from' => Carbon::parse($data[$i + 1]->created_at)->toDateTimeString(),
+                'to' => Carbon::parse($data[$i]->created_at)->toDateTimeString(),
                 'growth_per_day' => $plant_height_growth,
                 'unit' => 'cm/day'
             ]);
